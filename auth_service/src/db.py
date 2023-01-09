995984ebaa95bc1da2f8352780ae2958b510ebdc
valerycode <lera200@yandex.ru>
2023-01-10 01:18:53 +0600

    add models and views for roles

--- auth_service/src/db.py
+++ auth_service/src/db.py
@@ -1,5 +1,3 @@
-import os
-
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
@@ -7,5 +5,4 @@ db = SQLAlchemy()
 
 
 def init_db(app: Flask):
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('POSTGRES_URL')
     db.init_app(app)
